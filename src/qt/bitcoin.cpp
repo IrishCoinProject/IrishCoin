aea10ac82e008de4b38532263eb7ac4061f7acda
AnonymousPirate <LydiaCoin@riseup.net>
2014-04-27 19:38:22 -0400

    Update
    Final Commit

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -110,7 +110,7 @@ static std::string Translate(const char* psz)
 static void handleRunawayException(std::exception *e)
 {
     PrintExceptionContinue(e, "Runaway exception");
-    QMessageBox::critical(0, "Runaway exception", BitcoinGUI::tr("A fatal error occurred. Maplecoin can no longer continue safely and will quit.") + QString("\n\n") + QString::fromStdString(strMiscWarning));
+    QMessageBox::critical(0, "Runaway exception", BitcoinGUI::tr("A fatal error occurred. Irishcoin can no longer continue safely and will quit.") + QString("\n\n") + QString::fromStdString(strMiscWarning));
     exit(1);
 }
 
@@ -145,8 +145,8 @@ int main(int argc, char *argv[])
     if (!boost::filesystem::is_directory(GetDataDir(false)))
     {
         // This message can not be translated, as translation is not initialized yet
-        // (which not yet possible because lang=XX can be overridden in bitcoin.conf in the data directory)
-        QMessageBox::critical(0, "Maplecoin",
+        // (which not yet possible because lang=XX can be overridden in irishcoin.conf in the data directory)
+        QMessageBox::critical(0, "Irishcoin",
                               QString("Error: Specified data directory \"%1\" does not exist.").arg(QString::fromStdString(mapArgs["-datadir"])));
         return 1;
     }
@@ -154,12 +154,12 @@ int main(int argc, char *argv[])
 
     // Application identification (must be set before OptionsModel is initialized,
     // as it is used to locate QSettings)
-    QApplication::setOrganizationName("Maplecoin");
-    QApplication::setOrganizationDomain("www.maplecoin.info");
+    QApplication::setOrganizationName("Irishcoin");
+    QApplication::setOrganizationDomain("www.irishcoin.org");
     if(GetBoolArg("-testnet")) // Separate UI settings for testnet
-        QApplication::setApplicationName("Maplecoin-Qt-testnet");
+        QApplication::setApplicationName("Irishcoin-Qt-testnet");
     else
-        QApplication::setApplicationName("Maplecoin-Qt");
+        QApplication::setApplicationName("Irishcoin-Qt");
 
     // ... then GUI settings:
     OptionsModel optionsModel;
